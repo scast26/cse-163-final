6aec13ded44ef09d8df3ed603717b34cd4d9ca54
nmnobbs <77033963+nmnobbs@users.noreply.github.com>
2022-03-15 17:11:36 -0700

    plot comments!

--- question_1.py
+++ question_1.py
@@ -1,4 +1,9 @@
-# Plots to be refined
+"""
+This function takes a DataFrame containing songs and their audio
+features as a parameter. It returns a plot that shows the box and whisker
+plots of all of the features whose values range from zero to one.
+"""
+
 
 import plotly.express as px
 import plotly.subplots as sp
@@ -25,26 +30,47 @@ def filter_data(data):
     return df
 
 
-def facetting_scatter_plot(data, n_cols=3, y='Features'):
-    numeric_cols = data.select_dtypes('number').columns
-    # math.ceil in a fast way, without import
-    n_rows = -(-len(numeric_cols) // n_cols)
-    row_pos, col_pos = 1, 0
-    fig = sp.make_subplots(rows=n_rows, cols=n_cols,
-                           subplot_titles=numeric_cols)
+def scatter_plot(data, num_cols=3):
+    """
+    This function takes a DataFrame containing songs and their audio
+    features as a parameter as well as a number to determine how many
+    plots will be plotting in a single row. This returns many scatterplot
+    subplots that compare the presence and value of audio featues in
+    songs with a popularity score of 80 or above.
+    """
+    numeric_columns = data.select_dtypes('number').columns
+    # Calculate grid for plots
+    num_rows = -(-len(numeric_columns) // num_cols)
+    row_position, col_position = 1, 0
+    fig = sp.make_subplots(rows=num_rows, cols=num_cols,
+                           subplot_titles=numeric_columns)
 
-    for col in numeric_cols:
-        # trace extracted from the fig
+    for column in numeric_columns:
+        # Extracting trace from previously defined fig
         trace = px.scatter(
-            data, x=col, y=data['popularity'], trendline='ols', trendline_color_override='#DC143C')["data"]
-        # auto selecting a position of the grid
-        if col_pos == n_cols:
-            row_pos += 1
-        col_pos = col_pos + 1 if (col_pos < n_cols) else 1
-        # adding trace to the grid
-        fig.add_trace(trace[0], row=row_pos, col=col_pos)
-        fig.add_trace(trace[1], row=row_pos, col=col_pos)
+            data, x=column, y=data['popularity'], trendline='ols')["data"]
+        # Selecting a position of the grid
+        if col_position == num_cols:
+            row_position += 1
+        col_position = col_position + 1 if (col_position < num_cols) else 1
+        # Adding each trace to fig
+        fig.add_trace(trace[0], row=row_position, col=col_position)
+        fig.add_trace(trace[1], row=row_position, col=col_position)
     return fig
+
+
+def size_scatter(data):
+    """
+    This function takes a DataFrame containing songs and their audio
+    features as a parameter. This returns a colorful and size
+    senstiive scatterplot that compares the ratio of the values of
+    danceability to the values of energy with the size of each point
+    measuring the value of speechiness of a particular song with a
+    popularity score of 80 or above.
+    """
+    fig = px.scatter(data, x='danceability', y='energy', size='speechiness',
+                     color='popularity', log_x=True, size_max=60)
+    fig.show()
 
 
 def show_box_plots(data):
@@ -64,9 +90,19 @@ def show_box_plots(data):
 def main():
     df = filter_data(DATA)
 
-    fig = facetting_scatter_plot(df.select_dtypes(np.float64))
-    fig.update_layout(width=1000, height=800, title='Comparison of Song Features', title_x=0.5)
+    fig = scatter_plot(df.select_dtypes(np.float64))
+    fig.update_layout(width=1000, height=800,
+                      title='Comparison of Song Features', title_x=0.5)
     fig.show()
+
+    size_scatter(df)
+
+    box_plot = show_box_plots(df)
+    box_plot.show()
+
+
+if __name__ == '__main__':
+    main()
 
     box_plot = show_box_plots(df)
     box_plot.show()
